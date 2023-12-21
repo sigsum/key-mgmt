a12f3409bf4201dcb0dbe8c764e9b576cefe6875
Niels Möller <nisse@glasklarteknik.se>
2023-12-21 08:49:55 +0100

    Refactor ServeAgent, and increase arbitrary max size to 10000

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -14,12 +14,31 @@ const (
 	SSH_AGENTC_SIGN_REQUEST       = 13
 	SSH_AGENT_SIGN_RESPONSE       = 14
 	// Arbitrary maximum size of received agent messages.
-	maxSize = 1000
+	maxSize = 10000
 )
 
 // Returns signature formatted as an SSH signature (without outer
 // length field).
 type SSHSign func([]byte) ([]byte, error)
+
+type signRequest struct {
+	pubKey []byte
+	data   []byte
+}
+
+func readSignRequest(r io.Reader) (req signRequest, err error) {
+	req.pubKey, err = readString(r, maxSize)
+	if err != nil {
+		return
+	}
+	req.data, err = readString(r, maxSize)
+	if err != nil {
+		return
+	}
+	// Flags, currently ignored.
+	_, err = readUint32(r)
+	return
+}
 
 // The map keys are SSH public key blobs (without outer length field).
 func ServeAgent(r io.Reader, w io.Writer, keys map[string]SSHSign) error {
@@ -31,53 +50,42 @@ func ServeAgent(r io.Reader, w io.Writer, keys map[string]SSHSign) error {
 		if len(data) == 0 {
 			return fmt.Errorf("invalid empty agent message")
 		}
-		t, msg := data[0], bytes.NewBuffer(data[1:])
-		rsp := &bytes.Buffer{}
+		t, msg := data[0], data[1:]
+		var rsp bytes.Buffer
 		switch t {
 		case SSH_AGENTC_REQUEST_IDENTITIES:
-			if msg.Len() > 0 {
-				return fmt.Errorf("invalid message, %d left-over bytes in list request", msg.Len())
+			if len(msg) > 0 {
+				return fmt.Errorf("invalid message, %d left-over bytes in list request", len(msg))
 			}
 
 			if err := rsp.WriteByte(SSH_AGENT_IDENTITIES_ANSWER); err != nil {
 				return err
 			}
-			if err := writeUint32(rsp, uint32(len(keys))); err != nil {
+			if err := writeUint32(&rsp, uint32(len(keys))); err != nil {
 				return err
 			}
 			for k, _ := range keys {
-				if err := writeString(rsp, k); err != nil {
+				if err := writeString(&rsp, k); err != nil {
 					return err
 				}
 				// Arbitrary comment
-				if err := writeString(rsp, "oracle key"); err != nil {
+				if err := writeString(&rsp, "oracle key"); err != nil {
 					return err
 				}
 			}
 		case SSH_AGENTC_SIGN_REQUEST:
-			key, err := readString(msg, maxSize)
+			req, err := parseBytes(msg, nil, readSignRequest)
 			if err != nil {
 				return err
 			}
-			data, err := readString(msg, maxSize)
-			if err != nil {
-				return err
-			}
-			// Flags, currently ignored.
-			if _, err := readUint32(msg); err != nil {
-				return err
-			}
-			if msg.Len() > 0 {
-				return fmt.Errorf("invalid message, %d left-over bytes in sign request", msg.Len())
-			}
-			signer, ok := keys[string(key)]
+			signer, ok := keys[string(req.pubKey)]
 			if !ok {
 				if err := rsp.WriteByte(SSH_AGENT_FAILURE); err != nil {
 					return err
 				}
 				break
 			}
-			sig, err := signer(data)
+			sig, err := signer(req.data)
 			if err != nil {
 				log.Printf("signing failed: %v", err)
 				if err := rsp.WriteByte(SSH_AGENT_FAILURE); err != nil {
@@ -88,7 +96,7 @@ func ServeAgent(r io.Reader, w io.Writer, keys map[string]SSHSign) error {
 			if err := rsp.WriteByte(SSH_AGENT_SIGN_RESPONSE); err != nil {
 				return err
 			}
-			if err := writeString(rsp, sig); err != nil {
+			if err := writeString(&rsp, sig); err != nil {
 				return err
 			}
 		default:
